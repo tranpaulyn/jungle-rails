d9f453b7b88c000c7871d541bdf5f1891ab920f4
Paulyn <paulyntran@live.ca>
2019-03-22 18:25:16 +0000

    Re push last commit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
   http_basic_authenticate_with :name => ENV["HTTP_AUTH_USERNAME"], :password => ENV["HTTP_AUTH_PW"]
-    # http_basic_authenticate_with name: 'Jungle', password: 'book'
+
 
   def index
     @products = Product.order(id: :desc).all
